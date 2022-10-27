498162710f52e0a9527a755f9dfe751e2ee6975b
ahinkim <ahin98@naver.com>
2022-10-27 09:05:18 +0900

    feat: Add BOJ 1912

---
+++ BOJ/다이나믹/BOJ1912.py
@@ -0,0 +1,10 @@
+import sys
+input = lambda : sys.stdin.readline().rstrip()
+
+N = int(input())
+dp = list(map(int, input().split()))
+
+for i in range(1, N):
+    dp[i] = max(dp[i], dp[i-1] + dp[i])
+
+print(max(dp))
